592d54b83e07bddf166531446079eb617f75b03c
Michael Milton <ttmigueltt@gmail.com>
2019-03-20 18:17:11 +1100

    Update submit script to use curl

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
 # The first argument is the hostname to submit this to
 zip imports.zip b.wdl c.wdl d.wdl
-cromwell submit --host $1 --imports imports.zip a.wdl
+curl -X POST "$1/api/workflows/v1" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "workflowSource=@a.wdl" -F "workflowDependencies=@imports.zip;type=application/zip"
